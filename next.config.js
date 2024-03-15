53c3541f4d71c01e6a3b818e528f7d3d144bb3a5
Adit <aditdesai2002@gmail.com>
2024-03-16 03:27:50 +0530

    Allow HTTP request

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
     async headers() {
         return [
             {
-              source: '/:path*',
+              source: '/',
               headers: [
                {
                  key: 'Content-Security-Policy',
