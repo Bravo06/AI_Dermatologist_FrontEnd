ff75341521efbfc3bfef626b5c0776add26069ce
Adit <aditdesai2002@gmail.com>
2024-03-16 04:23:55 +0530

    Added proxy server

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
         return [
           {
             source: '/api/infer',
-            destination: 'http://ai-derm-api-lb-394270268.ap-south-1.elb.amazonaws.com/infer' // Proxy to Backend
+            destination: 'http://ai-derm-api-lb-394270268.ap-south-1.elb.amazonaws.com/infer',
+            permanent: true,
           }
         ]
       }
