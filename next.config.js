246cb6f6912168f7b385ef6effe6c9747676adc7
Adit <aditdesai2002@gmail.com>
2024-03-16 03:46:00 +0530

    Modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-              source: '/',
-              headers: [
-               {
-                 key: 'Content-Security-Policy',
-                 value: "upgrade-insecure-requests"
-               }
-              ],
-            },
-          ]
-      }
+    output: 'standalone'
 }
 
 module.exports = nextConfig
