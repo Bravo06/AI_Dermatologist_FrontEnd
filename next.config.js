fac73476327b3f5d6418bcfa237c21d256368a95
Adit <aditdesai2002@gmail.com>
2024-03-16 14:57:23 +0530

    Fuck this

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    async rewrites() {
+        return [
+          {
+            source: '/api/infer',
+            destination: 'http://ai-derm-api-lb-394270268.ap-south-1.elb.amazonaws.com/infer'
+          }
+        ]
+      }
 }
 
 module.exports = nextConfig
